326a471afd8ae42879eaac9a382654214692db2c
Nishikant Kanunje <nishikantkanunje546@gmail.com>
2025-09-25 21:16:18 +0530

    run emptyrecyclebin.sh as admin

--- emp.ps1
+++ emp.ps1
@@ -1,1 +1,1 @@
-Start-Process "C:\Program Files\Git\bin\bash.exe" -ArgumentList "--login -i 'C:/HDD_Backup/Scripts/emptyrecyclebin.sh'" -Verb RunAs
+Start-Process "C:\Program Files\Git\bin\bash.exe" -ArgumentList "--login -i 'D:/Scripts/emptyrecyclebin.sh'" -Verb RunAs
