85a04a11a975034fb041adbbe3ea3b62d44fe7c2
Nishikant Kanunje <nishikantkanunje546@gmail.com>
2025-09-26 19:59:00 +0530

    Format tester

---
+++ format-tester.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+echo -n -e "LF line\n" >Formatter_Tester.txt
+echo -n -e "LF line      \n" >>Formatter_Tester.txt
+echo -n -e "\r\n" >>Formatter_Tester.txt
+echo -n -e "\n" >>Formatter_Tester.txt
+echo -n -e "\r\n" >>Formatter_Tester.txt
+echo -n -e "\n" >>Formatter_Tester.txt
+echo -n -e "\r\n" >>Formatter_Tester.txt
+echo -n -e "\n" >>Formatter_Tester.txt
+echo -n -e "CRLF line        \r\n" >>Formatter_Tester.txt
+echo -n -e "CRLF line      \r\n" >>Formatter_Tester.txt
+echo -n -e "oneanr  soiente aste   anot oinaeirst                " >>Formatter_Tester.txt
+echo -n -e "Another LF      \n" >>Formatter_Tester.txt
+echo -n -e "CRLF line\r\n" >>Formatter_Tester.txt
+echo -n -e "\r\n" >>Formatter_Tester.txt
+echo -n -e "\n" >>Formatter_Tester.txt
+echo -n -e "\r\n" >>Formatter_Tester.txt
+echo -n -e "\n" >>Formatter_Tester.txt
+echo -n -e "\r\n" >>Formatter_Tester.txt
+echo -n -e "\n" >>Formatter_Tester.txt
+
+echo -n -e "LF line\n" >Formatter_Tester.sh
+echo -n -e "LF line      \n" >>Formatter_Tester.sh
+echo -n -e "\r\n" >>Formatter_Tester.sh
+echo -n -e "\n" >>Formatter_Tester.sh
+echo -n -e "\r\n" >>Formatter_Tester.sh
+echo -n -e "\n" >>Formatter_Tester.sh
+echo -n -e "\r\n" >>Formatter_Tester.sh
+echo -n -e "\n" >>Formatter_Tester.sh
+echo -n -e "CRLF line        \r\n" >>Formatter_Tester.sh
+echo -n -e "CRLF line      \r\n" >>Formatter_Tester.sh
+echo -n -e "oneanr  soiente aste   anot oinaeirst                " >>Formatter_Tester.sh
+echo -n -e "Another LF      \n" >>Formatter_Tester.sh
+echo -n -e "CRLF line\r\n" >>Formatter_Tester.sh
+echo -n -e "\r\n" >>Formatter_Tester.sh
+echo -n -e "\n" >>Formatter_Tester.sh
+echo -n -e "\r\n" >>Formatter_Tester.sh
+echo -n -e "\n" >>Formatter_Tester.sh
+echo -n -e "\r\n" >>Formatter_Tester.sh
+echo -n -e "\n" >>Formatter_Tester.sh
