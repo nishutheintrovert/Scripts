3a20289f9f8ab4faed725585d42886f0f30fcf2b
Nishikant Kanunje <nishikantkanunje546@gmail.com>
2025-09-26 20:25:04 +0530

    is text?

--- istext.sh
+++ istext.sh
@@ -7,10 +7,10 @@ RESET='\033[0m'
 echo -e "${RED}Text Files:\\n-----------${RESET}"
 
 # Find all text files in the current directory and its subdirectories
-find . -type f ! -path '*/.git/*' -exec file --mime-type {} + | grep -E 'text/' | cut -d: -f1
+find . -type f ! -path '*/.git/*' | xargs -r file --mime-encoding | grep -v 'binary' | cut -d: -f1
 
 # Print header for non-text files
 echo -e "${RED}NOT Text Files:\\n-----------${RESET}"
 
 # Find all non-text files in the current directory and its subdirectories
-find . -type f ! -path '*/.git/*' -exec file --mime-type {} + | grep -E -v 'text/' | cut -d: -f1
+find . -type f ! -path '*/.git/*' | xargs -r file --mime-encoding | grep 'binary' | cut -d: -f1
