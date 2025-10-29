e271af2e612829b8f0b603a4e1d651773db83585
Nishikant Kanunje <nishikantkanunje546@gmail.com>
2025-10-29 23:21:16 +0530

    removed unwanted scripts

--- emp.ps1
+++
@@ -1,1 +0,0 @@
-Start-Process "C:\Program Files\Git\bin\bash.exe" -ArgumentList "--login -i 'D:/Scripts/emptyrecyclebin.sh'" -Verb RunAs
--- emptyrecyclebin.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -rf '/c/$RECYCLE.BIN'
-rm -rf '/d/$RECYCLE.BIN'
-history -c && history -w
