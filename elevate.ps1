e810100290bd23616d5150fd96aa38940ab8efd8
Nishikant Kanunje <nishikantkanunje546@gmail.com>
2025-10-29 22:24:56 +0530

    Run scripts as admin

--- elevate.ps1
+++
@@ -1,3 +0,0 @@
-param([string]$Script)
-# Start-Process "C:\Program Files\Git\bin\bash.exe" -ArgumentList "--login", "-i", $Script -Verb RunAs
-Start-Process "C:\Program Files\Git\git-bash.exe" -ArgumentList $Script -Verb RunAs
--- sudo.sh
+++ sudo.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-powershell.exe -ExecutionPolicy Bypass -File "$(dirname "$0")/elevate.ps1" "$1"
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <script-to-run-as-admin>"
+    exit 1
+fi
+
+powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Start-Process 'C:\\Program Files\\Git\\git-bash.exe' -ArgumentList '$1' -Verb RunAs"
