e9f6ae591c79aeab05efeac80d0cd5f940e37f1b
Nishikant Kanunje <nishikantkanunje546@gmail.com>
2025-10-31 18:44:41 +0530

    Check extensions against .gitattributes

--- checkattributes.sh
+++ checkattributes.sh
@@ -13,19 +13,12 @@ RESET='\033[0m'
 # Delete empty files before processing (file --mime-encoding outputs binary for empty files regardless of extension)
 find . -type f ! -path '*/.git/*' -empty -print -delete
 
-# Collect all extensions into array (excluding .git)
-mapfile -t extensions < <(
-    find . -type f ! -path '*/.git/*' -print0 |
-        xargs -0 -r file -N --mime-encoding |
-        while IFS= read -r f; do echo "${f##*.}"; done
-)
-
-# Filter all extension to check against gitattributes
 echo -e "${YELLOW}Checking extensions against .gitattributes${RESET}"
-printf '%s\0' "${extensions[@]}" |
-    cut -zd: -f1 |
-    sort -zu |
-    while IFS= read -r -d '' ext; do
+
+find . -type f ! -path '*/.git/*' |
+    while IFS= read -r f; do echo "${f##*.}"; done |
+    sort -u |
+    while IFS= read -r ext; do
         if ! grep -q "^\*.$ext" .gitattributes 2>/dev/null; then
             echo -e "${RED}$ext${RESET}" # Print in red if not found
         fi
