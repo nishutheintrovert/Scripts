d237b79c2ec9c842345ac9e03e34d053cf24db25
Nishikant Kanunje <nishikantkanunje546@gmail.com>
2025-11-03 15:58:19 +0530

    Customize git-prompt

--- gitprompt.sh
+++ gitprompt.sh
@@ -1,22 +1,38 @@
 #!/bin/bash
 
+# Define ANSI color variables
+RED='\033[0;31m'
+GREEN='\033[0;92m'
+YELLOW='\033[0;93m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;95m'
+CYAN='\033[0;96m'
+WHITE='\033[0;97m'
+RESET='\033[0m'
+
 newgitprompt="/C/Program Files/Git/etc/profile.d/git-prompt.sh"
 oldgitprompt="/D/Config/old_git-prompt.sh"
 configgitprompt="/D/Config/git-prompt.sh"
 
-# Check that all files exist
+# Check that all files exist, print error for those who don't
 for file in "$newgitprompt" "$oldgitprompt" "$configgitprompt"; do
-    if [ ! -f "$file" ]; then
-        echo "Error: File not found: $file"
+    [ -f "$file" ] || {
+        echo -e "${RED}Error: $file not found${RESET}"
         read -rsn1
-    fi
+        exit 1
+    }
+    # Format files with shfmt, print formatted filenames
+    output=$(shfmt -l -w -i=4 -ci -ln=bash "$file")
+    [ -n "$output" ] && echo -e "${GREEN}Formatted: $output${RESET}"
+
 done
 
 # Compare newgitprompt against oldgitprompt
 if cmp -s "$newgitprompt" "$oldgitprompt"; then
     cp "$configgitprompt" "$newgitprompt"
+    echo -e "${GREEN}Files matched. Replaced git-prompt successfully.${RESET}"
     exit 0
 else
-    echo "Warning: $newgitprompt and $oldgitprompt differ. Not replacing."
+    echo -e "${RED}Warning: $newgitprompt and $oldgitprompt differ. Not replacing.${RESET}"
 fi
 read -rsn1
