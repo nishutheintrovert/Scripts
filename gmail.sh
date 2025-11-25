ab317fa8e5d8a5a37bb8c495b817c6c074190166
Nishikant Kanunje <nishikantkanunje546@gmail.com>
2025-11-26 00:23:38 +0530

    Reliably open gmail inboxes

--- gmail.sh
+++ gmail.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-# Easier oneliner
-# "/c/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe" https://mail.google.com/mail/u/{0..9}/#inbox
+# Define your browser path
+BROWSER="/c/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe"
 
-# Number of Gmail accounts to open
-NUM_ACCOUNTS=10
-
-# Loop through each account index and open the corresponding Gmail inbox
-for ((i = 0; i < NUM_ACCOUNTS; i++)); do
-    URL="https://mail.google.com/mail/u/$i/#inbox"
-    echo "Opening $URL"
-    # Start all URLs in one go
-    start "$URL" &
-done
-
-wait # optional: wait for all background processes
+# Bash expands {0..9} into 10 separate URLs automatically
+"$BROWSER" https://mail.google.com/mail/u/{0..9}/#inbox
