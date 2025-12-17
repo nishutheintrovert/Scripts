fdded5c847c99d24a21d4b8362def199de9c25ef
Nishikant Kanunje <nishikantkanunje546@gmail.com>
2025-12-18 04:49:59 +0530

    Open all gmail inboxes

--- gmail.sh
+++ gmail.sh
@@ -4,4 +4,5 @@
 BROWSER="/c/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe"
 
 # Bash expands {0..9} into 10 separate URLs automatically
-"$BROWSER" https://mail.google.com/mail/u/{0..9}/#inbox
+"$BROWSER" https://mail.google.com/mail/u/{0..9}/#inbox >/dev/null 2>&1 &
+disown
