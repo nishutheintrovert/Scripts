c1396ac18280cd8841c11cf67a23cf333a018316
Nishikant Kanunje <nishikantkanunje546@gmail.com>
2026-01-31 19:28:44 +0530

    Henrikdev endpoint test: mmr-history

--- Henrikdev_API/mmr-history.sh
+++ Henrikdev_API/mmr-history.sh
@@ -5,10 +5,10 @@ output_file="${script_name%.*}.json"
 
 region="ap"
 platform="pc"
-account="itsme/NISHU"
+puuid="86b05ddf-ed8b-50dd-994d-46769b29b66c"
 
 curl -L \
-    --url "https://api.henrikdev.xyz/valorant/v2/mmr-history/${region}/${platform}/${account}" \
+    --url "https://api.henrikdev.xyz/valorant/v2/by-puuid/mmr-history/${region}/${platform}/${puuid}" \
     --header "Authorization: ${HENRIKDEV_ADVANCED_KEY}" \
     --header 'Accept: application/json' \
     -o "${output_file}"
