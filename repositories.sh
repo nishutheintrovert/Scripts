a1555682e1e21d3c97f26240c7ba14194cf3a2e8
Nishikant Kanunje <nishikantkanunje546@gmail.com>
2026-04-28 01:45:37 +0530

    Store repositories locations with lf line endings safely to be sourced by GitST.sh, GitLog.sh, Repos.sh

---
+++ repositories.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+#    Author    : Nishikant Kanunje
+#    Date    : 28/04/2026
+#    Purpose    : Store repositories locations with lf line endings safely to be sourced by GitST.sh, GitLog.sh, Repos.sh
+
+directories=(
+    "/d/AHK"
+    "/d/Codespace"
+    "/d/College"
+    "/d/Config"
+    "/d/Documents"
+    "/d/Environment"
+    "/d/Registry"
+    "/d/Scripts"
+    "/d/Text"
+    "/d/skinview3d"
+    "/d/valorant-group-rank-fetcher"
+    "/d/Valorant-Rank-Fetcher"
+    "/d/Valorant-Settings-Syncer"
+)
