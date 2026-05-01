492a8506944e8e003b21d0e78afc42477e40a60a
Nishikant Kanunje <nishikantkanunje546@gmail.com>
2026-05-02 01:38:38 +0530

    Open bulk gmail links in browser

--- gmail.sh
+++ gmail.sh
@@ -4,5 +4,5 @@
 BROWSER="/c/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe"
 
 # Bash expands {0..9} into 10 separate URLs automatically
-"$BROWSER" https://mail.google.com/mail/u/{0..9}/#inbox >/dev/null 2>&1 &
+"$BROWSER" https://mail.google.com/mail/u/{0..4}/#inbox >/dev/null 2>&1 &
 disown
