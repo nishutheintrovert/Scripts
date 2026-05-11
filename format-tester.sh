00ff04416ecea0727e4b8c0ac0cb1899dc436c22
Nishikant Kanunje <nishikantkanunje546@gmail.com>
2026-05-12 04:51:58 +0530

    Reliable format.sh tester

--- format-tester.sh
+++ format-tester.sh
@@ -1,41 +1,39 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-echo -n -e "LF line\n" >Formatter_Tester.txt
-echo -n -e "LF line      \n" >>Formatter_Tester.txt
-echo -n -e "\r\n" >>Formatter_Tester.txt
-echo -n -e "\n" >>Formatter_Tester.txt
-echo -n -e "\r\n" >>Formatter_Tester.txt
-echo -n -e "\n" >>Formatter_Tester.txt
-echo -n -e "\r\n" >>Formatter_Tester.txt
-echo -n -e "\n" >>Formatter_Tester.txt
-echo -n -e "CRLF line        \r\n" >>Formatter_Tester.txt
-echo -n -e "CRLF line      \r\n" >>Formatter_Tester.txt
-echo -n -e "oneanr  soiente aste   anot oinaeirst                " >>Formatter_Tester.txt
-echo -n -e "Another LF      \n" >>Formatter_Tester.txt
-echo -n -e "CRLF line\r\n" >>Formatter_Tester.txt
-echo -n -e "\r\n" >>Formatter_Tester.txt
-echo -n -e "\n" >>Formatter_Tester.txt
-echo -n -e "\r\n" >>Formatter_Tester.txt
-echo -n -e "\n" >>Formatter_Tester.txt
-echo -n -e "\r\n" >>Formatter_Tester.txt
-echo -n -e "\n" >>Formatter_Tester.txt
+generate_messy_file() {
+    local target_file="$1"
 
-echo -n -e "LF line\n" >Formatter_Tester.sh
-echo -n -e "LF line      \n" >>Formatter_Tester.sh
-echo -n -e "\r\n" >>Formatter_Tester.sh
-echo -n -e "\n" >>Formatter_Tester.sh
-echo -n -e "\r\n" >>Formatter_Tester.sh
-echo -n -e "\n" >>Formatter_Tester.sh
-echo -n -e "\r\n" >>Formatter_Tester.sh
-echo -n -e "\n" >>Formatter_Tester.sh
-echo -n -e "CRLF line        \r\n" >>Formatter_Tester.sh
-echo -n -e "CRLF line      \r\n" >>Formatter_Tester.sh
-echo -n -e "oneanr  soiente aste   anot oinaeirst                " >>Formatter_Tester.sh
-echo -n -e "Another LF      \n" >>Formatter_Tester.sh
-echo -n -e "CRLF line\r\n" >>Formatter_Tester.sh
-echo -n -e "\r\n" >>Formatter_Tester.sh
-echo -n -e "\n" >>Formatter_Tester.sh
-echo -n -e "\r\n" >>Formatter_Tester.sh
-echo -n -e "\n" >>Formatter_Tester.sh
-echo -n -e "\r\n" >>Formatter_Tester.sh
-echo -n -e "\n" >>Formatter_Tester.sh
+    echo "Generating messy file: $target_file"
+
+    # \x20 = space | \t = tab | \n = LF | \r\n = CRLF
+
+    printf "LF line\n" >"$target_file"
+    printf "LF line\x20\x20\x20\x20\x20\x20\n" >>"$target_file"
+    printf "\r\n" >>"$target_file"
+    printf "\n" >>"$target_file"
+    printf "\r\n" >>"$target_file"
+    printf "\n" >>"$target_file"
+    printf "\r\n" >>"$target_file"
+    printf "\n" >>"$target_file"
+    printf "CRLF line\x20\x20\x20\x20\x20\x20\x20\x20\r\n" >>"$target_file"
+    printf "CRLF line\x20\x20\x20\x20\x20\x20\r\n" >>"$target_file"
+    printf "Line with tabs\t\t\tto test conversion\n" >>"$target_file"
+    printf "Trailing tabs test\t\t\n" >>"$target_file"
+    printf "oneanr\x20\x20soiente\x20aste\x20\x20\x20anot\x20oinaeirst\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20" >>"$target_file"
+    printf "Another LF\x20\x20\x20\x20\x20\x20\n" >>"$target_file"
+    printf "CRLF line\r\n" >>"$target_file"
+    printf "\r\n" >>"$target_file"
+    printf "\n" >>"$target_file"
+    printf "\r\n" >>"$target_file"
+    printf "\n" >>"$target_file"
+    printf "\r\n" >>"$target_file"
+    printf "\n" >>"$target_file"
+}
+
+generate_messy_file "test-text.txt"
+generate_messy_file "test-shell-script.sh"
+generate_messy_file ".test-dotfile"
+generate_messy_file "example.test-dotfile"
+generate_messy_file "test-no-extension-file"
+
+echo "Done! Test environment ready."
