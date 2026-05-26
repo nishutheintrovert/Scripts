6859de78a5fe5de0f1cdffac5d82ec1af2cc5452
Nishikant Kanunje <nishikantkanunje546@gmail.com>
2026-05-27 04:40:35 +0530

    MouseMover helper

---
+++ mousemover.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+echo "mousemover moveto $(mousemover position)" >>~/Desktop/temp.sh
+echo "read -t 0.5" >>~/Desktop/temp.sh
+echo "mousemover click" >>~/Desktop/temp.sh
+echo "" >>~/Desktop/temp.sh
--- neon.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "neon moveto $(neon position)" >>/d/desktop/temp.bat
-echo "timeout 1" >>/d/desktop/temp.bat
-echo "neon click" >>/d/desktop/temp.bat
-echo "" >>/d/desktop/temp.bat
