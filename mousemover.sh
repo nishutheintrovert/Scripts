7da14ce609d7aadbb6f833cd9ed193978cf5c825
Nishikant Kanunje <nishikantkanunje546@gmail.com>
2026-08-01 20:35:08 +0530

    MouseMover helper

--- mousemover.sh
+++ mousemover.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 
-echo "mousemover moveto $(mousemover position)" >>~/Desktop/temp.sh
-echo "read -t 0.5" >>~/Desktop/temp.sh
-echo "mousemover click" >>~/Desktop/temp.sh
-echo "" >>~/Desktop/temp.sh
+printf '\e[2t'
+
+printf -v output "%(%Y-%m-%d)T" -1
+output_file=~/Desktop/"$output".fsh
+
+if [ ! -f "$output_file" ]; then
+    echo -ne "#!/usr/bin/env bash\n\nprintf '\\\e[2t'\n" >>"$output_file"
+fi
+
+echo "" >>"$output_file"
+echo "mousemover moveto $(mousemover position)" >>"$output_file"
+echo "read -t 0.5" >>"$output_file"
+echo "mousemover click" >>"$output_file"
+mousemover click
