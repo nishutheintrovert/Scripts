ccc3f6c281a8a4830a8b04122a96b8f62d780d0e
Nishikant Kanunje <nishikantkanunje546@gmail.com>
2026-09-09 00:17:56 +0530

    MouseMover helper

--- mousemover.sh
+++ mousemover.sh
@@ -3,7 +3,11 @@
 printf '\e[2t'
 
 printf -v output "%(%Y-%m-%d)T" -1
-output_file=~/Desktop/"$output".fsh
+
+win_desktop_path=$(reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" //v Desktop | awk -F'    ' '/Desktop/{print $4}')
+DESKTOP_DIR=$(cygpath "$win_desktop_path")
+
+output_file="$DESKTOP_DIR/$output".fsh
 
 if [ ! -f "$output_file" ]; then
     echo -ne "#!/usr/bin/env bash\n\nprintf '\\\e[2t'\n" >>"$output_file"
